8e8e6cae44526510b0fa65a18e2a538789dbefbb
tonyhzw <tony_join@163.com>
2017-03-29 11:44:58 +0800

    修改了markdown的展示

--- src/views/newPost.vue
+++ src/views/newPost.vue
@@ -1,22 +1,38 @@
 <template>
   <div class="post">
-    <Input v-model="formItem.input" class="title" placeholder="请输入标题"></Input>
-     <div class="navbar"></div>
-     <Row class="row">
-       <Col span="12">
-         <div class="inputArea"></div>
+    <div class="top">
+        <Input v-model="formItem.input" class="title" name="title" placeholder="请输入标题"></Input>
+        <button class="submit" @click="submitPost">提交</button>
+    </div>
+     <div class="navbar">
+        <span>标签</span>
+        <div>
+        <Input class="category" v-model="formItem.category" name="category" placeholder="请输入类别，以空格间隔"></Input>
+        </div>
+     </div>
+     <div class="row">
          <textarea v-model="formItem.textarea" rows="5" class="content" placeholder="请输入..."></textarea>
-       </Col>
-       <Col span="12">
-         <div class="markdownResult markdown-body" v-html="htmlStr"></div>
-       </Col>
-     <Row>
+         <div class="markdown-container">
+            <div class="markdown-body" v-html="htmlStr"></div>
+         </div>
+     <div>
   </div>
 </template>
 <style scoped>
- @import "./../css/github-markdown.css"
+ @import "./../css/github-markdown.css";
+ @import "./../css/default.css";
 .post{
   min-height: 486px;
+}
+.top{
+  position: relative;
+}
+.submit{
+  position: absolute;
+  top: 6px;
+  right: 30px;
+  border: none;
+  padding: 10px 30px;
 }
 .title{
   font-size: 20px;
@@ -27,9 +43,36 @@
 .row{
   position: relative;
 }
+.row .markdown-container{
+  margin-left: 50%;
+}
+.row:after{
+   content: '';
+   display: block;
+   clear: both;
+   height: 0;
+   visibility: hidden;
+}
 .navbar{
-    height: 30px;
-    background-color: #ededed;
+  height: 40px;
+  background-color: #ededed;
+}
+.navbar span{
+  line-height: 40px;
+  margin-left: 10px;
+}
+.navbar div{
+  margin-left: 45px;
+  margin-top: -40px;
+}
+.navbar div .category{
+  line-height: 40px;
+  width: 100%;
+  padding-left: 10px;
+  border:none;
+  border-radius: 0;
+  outline: none;
+  background: rgba(255,255,255,0.5);
 }
 .inputArea{
    min-height: 400px;
@@ -37,10 +80,7 @@
 }
 .content{
    font-size: 18px;
-   position: absolute;
-   top: 0;
-   left: 0;
-   z-index: 3;
+   float: left;
    padding: 12px;
    width: 50%;
    background: transparent;
@@ -52,11 +92,6 @@
    -webkit-user-select: text;
    user-select: text;
 }
-.markdownResult{
-    position: absolute;
-    left: 50%;
-    top: 0;
-}
 .markdown-body {
 		box-sizing: border-box;
 		min-width: 200px;
@@ -67,23 +102,34 @@
 </style>
 <script>
 import axios from 'axios'
-import {markdown} from 'markdown'
+import autosize from 'autosize'
+import showdown from 'showdown'
+import hljs from 'highlight.js'
+
 export default{
+    ready(){
+        autosize(document.querySelectorAll('textarea.content'));
+        hljs.initHighlightingOnLoad();
+    },
     data(){
         return{
           formItem: {
                   input:'',
+                  category:'',
                   textarea: ''
               }
         }
     },
     computed:{
        htmlStr:function(){
-          return markdown.toHTML(this.formItem.textarea);
+          let converter = new showdown.Converter();
+          return converter.makeHtml(this.formItem.textarea);
        }
     },
     methods:{
+      submitPost:function() {
 
+      }
     }
 }
 </script>
